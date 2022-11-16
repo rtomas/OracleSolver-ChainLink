cff27676c8276d41a4fee15104c6eb7c2a2395b3
Tomas Rawski <rtomas@gmail.com>
2022-11-16 14:06:12 -0300

    add ownable

--- contracts/OracleSolver.sol
+++ contracts/OracleSolver.sol
@@ -3,13 +3,14 @@
 pragma solidity ^0.8.4;
 
 import "./PriceConsumerV3.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title OracleSolver
  * @dev Given two tokens it solves address of the chainlink oracle
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
-contract OracleSolver {
+contract OracleSolver is Ownable {
   mapping(bytes32 => address) public tokensSolver;
   mapping(bytes32 => PriceConsumerV3) public tokensPricer; //this should be private
 
@@ -22,7 +23,7 @@ contract OracleSolver {
     address tokenA,
     address tokenB,
     address solver
-  ) public {
+  ) public onlyOwner {
     address pairContract = getContractSolver(tokenA, tokenB);
     if (pairContract != address(0)) revert PairExist(tokenA, tokenB, pairContract);
 
@@ -30,17 +31,6 @@ contract OracleSolver {
 
     tokensPricer[tokens] = new PriceConsumerV3(solver); // create a new solver contract
     tokensSolver[tokens] = solver;
-  }
-
-  function orderAddress(address tokenA, address tokenB) private pure returns (address, address) {
-    return tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-  }
-
-  function generateTokensBytes(address tokenA, address tokenB) private pure returns (bytes32) {
-    (address token0, address token1) = orderAddress(tokenA, tokenB);
-    if (token0 == address(0)) revert ZeroAddress(token0);
-
-    return keccak256(abi.encodePacked(token0, token1));
   }
 
   function getContractSolver(address tokenA, address tokenB) public view returns (address) {
@@ -62,5 +52,16 @@ contract OracleSolver {
     if (pairContract == address(0)) revert PairNotExist(tokenA, tokenB);
 
     return getContractConsumer(tokenA, tokenB).getLatestPrice();
+  }
+
+  function orderAddress(address tokenA, address tokenB) private pure returns (address, address) {
+    return tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+  }
+
+  function generateTokensBytes(address tokenA, address tokenB) private pure returns (bytes32) {
+    (address token0, address token1) = orderAddress(tokenA, tokenB);
+    if (token0 == address(0)) revert ZeroAddress(token0);
+
+    return keccak256(abi.encodePacked(token0, token1));
   }
 }
